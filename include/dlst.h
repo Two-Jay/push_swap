ec11d2d3f798952f62569bd75e0468ca6fc759f5
jekim <arabi1549@naver.com>
2021-05-19 18:41:48 +0900

    dlst v 1.0.0 complete

--- include/dlst.h
+++ include/dlst.h
@@ -6,7 +6,7 @@
 /*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 16:02:06 by jekim             #+#    #+#             */
-/*   Updated: 2021/05/19 17:02:16 by jekim            ###   ########.fr       */
+/*   Updated: 2021/05/19 18:41:20 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,13 @@ typedef struct	s_dlst
 	t_dlst	*before;
 }				t_dlst;
 
-t_dlst	*ft_dlstnew(void *value);
+t_dlst			*ft_dlstnew(void *value);
+t_dlst			*ft_dlstpop_back(t_stack *stack);
+t_dlst			*ft_dlstpop_front(t_stack *stack);
+int				ft_dlstadd_back(t_stack *stack, t_dlst *node);
+int				ft_dlstadd_front(t_stack *stack, t_dlst *node);
+t_dlst			*ft_dlstnext(t_dlst *node, unsigned int step);
+t_dlst			*ft_dlstback(t_dlst *node, unsigned int step);
+unsigned int	ft_dlst_idxof(t_stack *stack, int target); // if the func fails, return -1
 
 #endif
--- srcs/dlst/dlst.c
+++ srcs/dlst/dlst.c
@@ -6,22 +6,131 @@
 /*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 15:10:22 by jekim             #+#    #+#             */
-/*   Updated: 2021/05/18 21:50:21 by jekim            ###   ########.fr       */
+/*   Updated: 2021/05/19 18:39:26 by jekim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 #include "../../include/dlst.h"
 
-void	ft_dlstadd_front(t_stack *stack, t_dlst *newnode)
+int				ft_dlstadd_front(t_stack *stack, t_dlst *node)
 {
 	t_dlst *tmp;
 
-	tmp = 0;
-	if (!stack || !newnode)
-		return ;
+	tmp = stack->top;
+	if (!stack || !node)
+		return (EXIT_FAILURE);
+	node->next = tmp;
+	node->before = stack->bottom;
+	stack->bottom->next = node;
+	stack->top = node;
+	tmp->before = node;
+	stack->size += 1;
+	return (EXIT_SUCCESS);
+}
+
+int				ft_dlstadd_back(t_stack *stack, t_dlst *node)
+{
+	t_dlst *tmp;
+
+	tmp = stack->bottom;
+	if (!stack || !node)
+		return (EXIT_FAILURE);
+	node->before = tmp;
+	node->next = stack->top;
+	stack->top->before = node;
+	stack->bottom = node;
+	tmp->next = node;
+	stack->size += 1;
+	return (EXIT_SUCCESS);
+}
+
+unsigned int	ft_dlst_idxof(t_stack *stack, int target)
+{
+	int ix;
+	t_dlst	*ptr;
 	
-	stack->top->next = newnode;
+	if (!stack)
+		return (-1);
+	ix = 0;
+	ptr = stack->top;
+	while (ptr && ix < stack->size)
+	{
+		if (ptr->value == target)
+			return (ix);
+		else
+		{
+			ptr = ptr->next;
+			ix++;
+		}
+	}
+	return (-1);
+}
+
+t_dlst			*ft_dlstpop_front(t_stack *stack)
+{
+	t_dlst *ret;
+	t_dlst *newtop;
+	
+	if (!stack || stack->size == 0)
+		return ;
+	ret = stack->top;
+	newtop = stack->top->next;
+	stack->top = newtop;
+	stack->bottom->next = newtop;
+	newtop->before = stack->bottom;
+	ret->next = NULL;
+	ret->before = NULL;
+	stack->size -= 1;
+	return (ret);
+}
+
+t_dlst			*ft_dlstpop_back(t_stack *stack)
+{
+	t_dlst	*ret;
+	t_dlst	*newbottom;
+
+	if (!stack || stack->size == 0)
+		return ;
+	ret = stack->bottom;
+	newbottom = stack->bottom->before;
+	stack->bottom = newbottom;
+	stack->top->before = newbottom;
+	newbottom->next = stack->top;
+	ret->next = NULL;
+	ret->before = NULL;
+	stack->size -= 1;
+	return (ret);
+}
+
+t_dlst			*ft_dlstnext(t_dlst *node, unsigned int step)
+{
+	int		ix;
+	t_dlst	*ptr;
+	
+	ix = 0;
+	ptr = node;
+	while (ptr->next && ix < step)
+	{
+		ptr = ptr->next;
+		ix++;
+	}
+	return (ptr);
+}
+
+t_dlst			*ft_dlstback(t_dlst *node, unsigned int step)
+{
+	int		ix;
+	t_dlst	*ptr;
+	
+	ix = 0;
+	ptr = node;
+	while (ptr->before && ix < step)
+	{
+		ptr = ptr->before;
+		ix++;
+	}
+	return (ptr);
 }
 
 t_dlst	*ft_dlstnew(void *value)
