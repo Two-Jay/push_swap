d7d9ae860cc9e51459be315f08566820cdfc2e96
Two-Jay <djeeee1272@gmail.com>
2021-07-08 12:10:46 +0900

    t_data newly defined in push_swap.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jekim <jekim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 19:37:26 by jekim             #+#    #+#             */
-/*   Updated: 2021/07/05 11:28:49 by jekim            ###   ########.fr       */
+/*   Updated: 2021/07/08 12:10:04 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,22 @@
 #include <stdlib.h>
 #include "../lib/libft/libft.h"
 #include "../lib/libps/libps.h"
+
+typedef struct	s_data
+{
+	t_dlst *a_stack;
+	t_dlst *a_top;
+	t_dlst *a_bottom;
+
+	t_dlst *b_stack;
+	t_dlst *b_top;
+	t_dlst *b_bottom;
+
+	int *input_arr;
+	int	size_all;
+	int	size_a;
+	int size_b;
+	int result;
+}				t_data;
 
 #endif
