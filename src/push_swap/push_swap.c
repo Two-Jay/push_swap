f074cff000934b633250f46eeead395a7dae8b43
Two-Jay <djeeee1272@gmail.com>
2021-07-10 12:29:46 +0900

    fn_dupcheck created

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 19:54:08 by jekim             #+#    #+#             */
-/*   Updated: 2021/07/10 09:26:28 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/07/10 12:28:33 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,25 @@
 // checked!
 // int ft_validate_input(int argc, char **argv, t_bucket *data);
 // duplicated input check, if err return 1, or if not 0
-// void	fn_check_dupvalue(int value, t_stack *stack)
-// {
+int	fn_check_dupvalue(int value, t_stack *stack)
+{
+	int ix;
+	t_dlst *ptr;
 	
-// }
+	ix = 0;
+	ptr = stack->top;
+	while (ix < stack->size)
+	{
+		if (value == ptr->value)
+			ft_strerr("Error\n duplicated values exist");
+		else
+		{
+			ptr = ptr->next;
+			ix++;
+		}
+	}
+	return (0);
+}
 
 void fn_print_stack(t_bucket *data)
 {
@@ -28,12 +43,10 @@ void fn_print_stack(t_bucket *data)
 	ps_dlstprint(data->b);
 }
 
-void	fn_insert_node(char *str, int ix, int *err_flag, t_bucket *data)
+void	fn_insert_node(int value, int ix, t_bucket *data)
 {
 	t_dlst *node;
-	int value;
 
-	value = ft_atoi_covf(str, err_flag);
 	node = ps_dlstnew(value);
 	ps_dlstadd_back(data->a, node);
 	data->input_arr[ix] = value;
@@ -42,9 +55,11 @@ void	fn_insert_node(char *str, int ix, int *err_flag, t_bucket *data)
 int fn_validate_input(int argc, char **argv, t_bucket *data)
 {
 	int	ix;
+	int value;
 	int	err_flag;
 
 	ix = 0;
+	value = 0;
 	err_flag = 0;
 	if (argc == 1)
 		ft_strerr("Error\n : a few parameters\n");
@@ -52,8 +67,12 @@ int fn_validate_input(int argc, char **argv, t_bucket *data)
 	{
 		err_flag = ft_isable_strtonbr(argv[ix + 1]);
 		if (!err_flag)
-			fn_insert_node(argv[ix + 1], ix, &err_flag, data);
-			// data->input_arr[ix] = ft_atoi_covf(argv[ix + 1], &err_flag); // arr에 넣어 준 이후에, 노드 생성하고 A 스텍에 넣기			
+		{
+			value = ft_atoi_covf(argv[ix + 1], &err_flag);
+			fn_check_dupvalue(value, data->a);
+			fn_insert_node(value, ix, data);
+		}
+		// data->input_arr[ix] = ft_atoi_covf(argv[ix + 1], &err_flag); // arr에 넣어 준 이후에, 노드 생성하고 A 스텍에 넣기			
 		if (err_flag)
 			ft_strerr("Error\n: an invalid param");
 		ix++;
