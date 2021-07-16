81b02fa399eee77e257fc082322398147c3e273b
Two-Jay <djeeee1272@gmail.com>
2021-07-17 04:25:40 +0900

    sb fixed

--- lib/libps/ps_inst_sb.c
+++ lib/libps/ps_inst_sb.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 19:21:14 by jekim             #+#    #+#             */
-/*   Updated: 2021/07/16 23:48:17 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/07/17 04:23:51 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 void	ps_inst_sb(t_bucket *bucket)
 {
-	int old_top_value;
-	int new_top_value;
+	t_dlst	*first;
+	t_dlst	*second;
 
 	if (bucket->a->size < 2)
 		return ;
-	old_top_value = bucket->b->top->value;
-	new_top_value = bucket->b->top->next->value;
-	ps_dlstpop_front(bucket->b);
-	ps_dlstpop_front(bucket->b);
-	ps_dlstadd_front(bucket->b, ps_dlstnew(old_top_value));
-	ps_dlstadd_front(bucket->b, ps_dlstnew(new_top_value));
+	first = ps_dlstpop_front(bucket->b);
+	second = ps_dlstpop_front(bucket->b);
+	ps_dlstadd_front(bucket->b, first);
+	ps_dlstadd_front(bucket->b, second);
 	bucket->count++;
 }
