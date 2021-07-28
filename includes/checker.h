211596f6f0bac1c68fa73f67db9fdd2e66b59e54
Two-Jay <djeeee1272@gmail.com>
2021-07-28 11:42:35 +0900

    retry

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/26 19:37:26 by jekim             #+#    #+#             */
-/*   Updated: 2021/07/28 03:39:20 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/07/28 11:42:15 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@
 # include "../lib/libft/libft.h"
 # include "../lib/libps/libps.h"
 
+# define STR_ARG 1
+# define NUM_ARG 2
+
 int		fn_bubblesort(int *arr, int size);
 int		fn_is_sorted(int *arr, int size);
 void	fn_clear_memory(t_bucket *data);
-int		fn_validate_input(int argc, char **argv, t_bucket *data);
+void	fn_validate_input(int argc, char **argv, t_bucket *data);
 void	fn_print_stack(t_bucket *data, int flag);
 
 void	fn_check_setting_type(int argc, char **argv, t_bucket *data);
