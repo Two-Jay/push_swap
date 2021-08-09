c936e2f60bb20efdb38c5135971a85765e76771b
Two-Jay <djeeee1272@gmail.com>
2021-08-09 19:30:52 +0900

    edited push_swap algo to remove an unnecessary code

--- src/push_swap/swap2.c
+++ src/push_swap/swap2.c
@@ -6,7 +6,7 @@
 /*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 06:54:36 by jekim             #+#    #+#             */
-/*   Updated: 2021/07/29 04:10:54 by jekim            ###   ########seoul.kr  */
+/*   Updated: 2021/08/09 19:27:25 by jekim            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,6 @@ static int	fn_sweep_value_into_chunk(t_bucket *data,
 	return (1);
 }
 
-static void	fn_put_front_lowest_atop_to_b(t_bucket *data)
-{
-	static int	lowest_rank;
-
-	if (data->b->size == 0)
-	{
-		ps_inst_pb(data, 0);
-		lowest_rank = data->b->top->rank;
-	}
-	if (data->b->size == 1)
-	{
-		if (data->a->top->rank < data->b->top->rank)
-			lowest_rank = data->a->top->rank;
-		ps_inst_pb(data, 0);
-		if (data->b->top->rank == lowest_rank)
-			ps_inst_sb(data, 0, 0);
-	}
-	else if (data->a->top != NULL)
-		ps_inst_pb(data, 0);
-}
-
 int	fn_push_swap_t100(t_bucket *data, int chunk_nbr, int chunk_size)
 {
 	int	ix;
@@ -69,7 +48,7 @@ int	fn_push_swap_t100(t_bucket *data, int chunk_nbr, int chunk_size)
 		while (jx < chunk_size)
 		{
 			fn_sweep_value_into_chunk(data, chunk_size, ix);
-			fn_put_front_lowest_atop_to_b(data);
+			ps_inst_pb(data, 0);
 			jx++;
 		}
 		ix++;
@@ -96,7 +75,7 @@ int	fn_push_swap_o100(t_bucket *data, int chunk_nbr, int chunk_size)
 		while (jx < chunk_size)
 		{
 			fn_sweep_value_into_chunk(data, chunk_size, ix);
-			fn_put_front_lowest_atop_to_b(data);
+			ps_inst_pb(data, 0);
 			jx++;
 		}
 		ix++;
