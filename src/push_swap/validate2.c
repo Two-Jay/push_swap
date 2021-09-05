48abe4330a7ea21c3e5c9e2740a3caef5c6ce357
Two-Jay <djeeee1272@gmail.com>
2021-09-05 19:54:09 +0900

    check

---
+++ src/push_swap/validate2.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jekim <arabi1549@naver.com>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/18 15:47:48 by jekim             #+#    #+#             */
+/*   Updated: 2021/08/20 01:14:31 by jekim            ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+int	fn_set_argv(int argc, char **argv, t_bucket *data)
+{
+	int		ix;
+	int		jx;
+	int		zx;
+	char	**tmp;
+
+	zx = 0;
+	ix = 1;
+	data->input_arr_str = (char **)ft_calloc(sizeof(char *), 10000);
+	if (argc <= 1)
+		return (ft_strerr("Error\n"));
+	while (ix < argc)
+	{
+		jx = 0;
+		tmp = ft_split(argv[ix], ' ');
+		while (tmp[jx])
+			data->input_arr_str[zx++] = tmp[jx++];
+		free(tmp);
+		ix++;
+	}
+	data->arg_size = zx;
+	return (0);
+}
